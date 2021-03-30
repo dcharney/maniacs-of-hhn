194523001f273eb9f6548ae4af4d2f43145a87a7
Delaney <33069490+dcharney@users.noreply.github.com>
2021-03-31 00:42:24 +0200

    Develop (#1)
    * basic server set up complete
    
    * cra client built and root npm set up, app can now be run from root using npm start

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const client = new ApolloClient({
+    request: operation => {
+        const token = localStorage.getItem('id_token');
+
+        operation.setContext({
+            headers: {
+                authorization: token ? `Bearer ${token}` : ''
+            }
+        })
+    },
+    uri: '/graphql'
+});
+
+function App() {
+
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <div className="App">
+                    <Header />
+                    <div className='container'>
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                        </Switch>
+                    </div>
+                    <Footer />
+                </div>
+            </Router>
+        </ApolloProvider>
+    );
+}
+
+export default App;
