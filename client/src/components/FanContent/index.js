bfaaacbcb76d78c6d00f98837b26a290c092e508
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-04-05 22:01:44 -0400

    setting up fancontent page

---
+++ client/src/components/FanContent/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+
+
+const FanContent = () => {
+
+
+    return (
+        <div>
+            
+        </div>
+
+    );
+
+};
+
+export default FanContent;
---
+++ client/src/pages/FanContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FanContent from '../components/FanContent';
+
+const FanContent = () => {
+    return (
+        <main>
+            <FanContent />
+        </main>
+    );
+};
+
+export default FanContent;
