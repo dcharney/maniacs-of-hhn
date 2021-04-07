c3f94e4419530e2dbc44137ed2c09b3d4017492b
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-04-07 16:14:44 -0400

    added ingreditents sections, added submitting the following, added handle change

--- client/src/components/FanContent/index.js
+++ client/src/components/FanContent/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import './style.css';
 
 // function clickMe(){
@@ -21,8 +21,16 @@ import './style.css';
 
 // };
 
+const formReducer = (state, event) => {
+    return {
+      ...state,
+      [event.name]: event.value
+    }
+   }
+
 
 function FanContent() {
+    const [formData, setFormData] = useReducer(formReducer, {});
     const [submitting, setSubmitting] = useState(false);
     const handleSubmit = event => {
         event.preventDefault();
@@ -33,18 +41,38 @@ function FanContent() {
         }, 3000)
       }
 
+      const handleChange = event => {
+        setFormData({
+          name: event.target.name,
+          value: event.target.value,
+        });
+      }
+
     return(
         <div className="wrapper">
           <h1>Recipes</h1>
           {submitting &&
-       <div>Submtting Form...</div>
-     }
+       <div>
+         You are submitting the following:
+         <ul>
+           {Object.entries(formData).map(([name, value]) => (
+             <li key={name}><strong>{name}</strong>:{value.toString()}</li>
+           ))}
+         </ul>
+       </div>
+      }
           <form onSubmit={handleSubmit}>
           <fieldset>
              <label>
                <p>Recipes Name</p>
-               <input name="name" />
+               <input name="name" onChange={handleChange}/>
              </label>
+           </fieldset>
+           <fieldset>
+         <label>
+           <p>Ingredients</p>
+           <input name="ingredients" onChange={handleChange}/>
+           </label>
            </fieldset>
            <button type="submit">Submit</button>
           </form>
