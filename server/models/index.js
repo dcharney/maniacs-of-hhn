c624e3b8d8d332fc862f89ad27782a32452ea8b7
Nick May <nickmay9@outlook.com>
2021-04-08 09:12:10 -0400

    exported all models

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,7 @@
 const User = require('./User');
+const Attraction = require('./Attraction');
+const Post = require('./Post');
+const Rating = require('./Rating');
+const Comment = require('./Comments');
 
-module.exports = { User };
+module.exports = { User, Attraction, Post, Rating, Comment };
