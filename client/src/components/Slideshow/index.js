5e8d51298fe2114063ec2894674a2cbab2939cb7
Tabatha Bell <tabathambell@live.com>
2021-04-11 15:26:46 -0400

    finished slideshow on main page and added it to the home page beneath the header

--- client/src/components/Slideshow/index.js
+++ client/src/components/Slideshow/index.js
@@ -1,33 +1,83 @@
-import React from 'react';
-import { Slide } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css'
+import React, {useState, useEffect} from 'react';
 
-const slideImages = [
-  'https://i.imgur.com/iusXJ37.jpg',
-];
+import './styles.css';
 
-const Slideshow = () => {
+export default function Slideshow({images=['https://i.imgur.com/TRFClRl.jpg', 'https://i.imgur.com/UnfU3Xj.jpg', 'https://i.imgur.com/BzY2we4.jpg', 'https://i.imgur.com/3P4IIGj.jpg', 'https://i.imgur.com/kOp8gqH.jpg'], interval=4000}){
+    const [thumbnails, setThumnails] = useState([]);
+    const [previousSlideStyle, setPreviousSlideStyle] = useState({});
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [nextSlideStyle, setNextSlideStyle] = useState({});
+    const [currentSlideStyle, setCurrentSlideStyle] = useState({});
+
+    useEffect(()=>{
+        setThumnails(images);
+        setCurrentSlideStyle({
+            backgroundImage: "url('"+images[currentSlide]+"')"
+        });
+
+        if(currentSlide>0){
+            setPreviousSlideStyle({
+                backgroundImage: "url('"+images[currentSlide-1]+"')"
+            });
+        }else{
+            setPreviousSlideStyle({
+                backgroundImage: "url('"+images[images.length-1]+"')"
+            });
+        }
+
+        if(currentSlide === images.length-1){
+            setNextSlideStyle({
+                backgroundImage: "url('"+images[0]+"')"
+            });
+        }else{
+            setNextSlideStyle({
+                backgroundImage: "url('"+images[currentSlide+1]+"')"
+            });
+        } 
+
+        const loop = setInterval(()=>{
+            if(currentSlide === images.length-1){
+                setCurrentSlide(0);
+            }else{
+                setCurrentSlide(currentSlide+1);
+            }
+        }, interval);
+        return () => clearInterval(loop); 
+    }, [currentSlide]);
+
+    function previous(){
+      console.log("worked")
+        if(currentSlide>0){
+            setCurrentSlide(currentSlide-1);
+        }else{
+            setCurrentSlide(thumbnails.length-1);
+        }
+    }
+
+    function next(){
+      console.log("workednext")
+      console.log(currentSlide);
+      console.log(thumbnails);
+        if(currentSlide === thumbnails.length-1){
+            setCurrentSlide(0);
+        }else{
+            setCurrentSlide(currentSlide+1);
+        }
+    }
+
     return (
-      <div className="slide-container">
-        <Slide>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-              <span>Slide 1</span>
+        <section className="slideshow">
+            <div className="slide-holder">
+                <section className="slide previous-slide">
+                    <div style={previousSlideStyle} className="slide-thumbnail"></div>
+                </section>
+                <section className="slide current-slide">
+                    <div style={currentSlideStyle} className="slide-thumbnail"></div>
+                </section>
+                <section className="slide next-slide">
+                    <div style={nextSlideStyle} className="slide-thumbnail"></div>
+                </section>
             </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-              <span>Slide 2</span>
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-              <span>Slide 3</span>
-            </div>
-          </div>
-        </Slide>
-      </div>
+        </section>
     )
 }
-
-export default Slideshow;
