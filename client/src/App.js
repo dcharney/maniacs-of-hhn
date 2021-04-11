80f4750af78543b5a3422df01ad1609b2466b8de
Tabatha Bell <tabathambell@live.com>
2021-04-10 21:55:56 -0400

    beginning slideshow, unsure what's going on with it's length or why the css isn't working

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import Explore from './pages/Explore';
 import Chat from './components/ChatRoom';
+import Slideshow from './components/Slideshow';
 
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
@@ -31,6 +32,7 @@ function App() {
                 <div className="App">
                     <Header />
                     <div id='content'>
+                            <Slideshow />
                         <Switch>
                             <Route exact path="/" component={Home} />
                             <Route exact path="/explore" component={Explore} />
