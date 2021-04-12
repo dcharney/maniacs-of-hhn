f6c8d466e01115e84962a2d5338f0bebdc32d954
Michael Arvelo <michaeldevelopscode@gmail.com>
2021-04-12 13:06:12 -0400

    added linking google data to firebase user data, todo: next link to our db

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useMutation } from '@apollo/react-hooks';
 import { Link } from "react-router-dom";
 import { LOGIN } from "../utils/mutations";
 import Auth from "../utils/auth";
@@ -21,7 +22,7 @@ if (firebase.apps.length === 0) {
     appId: "1:196282215934:web:8a3f1b10e1a4521e2124af",
     measurementId: "G-YS6TVWM8T6"
   })
-}
+};
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
@@ -48,6 +49,60 @@ function Login(props) {
       [name]: value
     });
   };
+
+  async function promptUserForPassword() {
+    const password = await new Promise((resolve) => {
+      const message = "User already exists, enter password associated with this email";
+      const res = prompt(message);
+
+      resolve(res);
+    });
+
+    if(password) {
+      return(password);
+    }
+  }
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+        // Grab Google Access Token to access the Google API.
+        var token = credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+      }).catch((error) => {
+        // if user exists with this email already but wasn't linked
+        if (error.code === 'auth/account-exists-with-different-credential') {
+          // The pending Google credential.
+          var pendingCred = error.credential;
+          // The provider account's email address.
+          var email = error.email;
+          // Get sign-in methods for this email.
+          auth.fetchSignInMethodsForEmail(email).then(function(methods) {
+            // Step 3.
+            // If the user has several sign-in methods,
+            // the first method in the list will be the "recommended" method to use.
+            if (methods[0] === 'password') {
+              // Asks the user their password.
+              var password = promptUserForPassword(); // implemented promptUserForPassword.
+              //use info to sign in
+              auth.signInWithEmailAndPassword(email, password).then(function(result) {
+                // Step 4a.
+                return result.user.linkWithCredential(pendingCred);
+              }).then(function() {
+                // Google account successfully linked to the existing Firebase user.
+                goToApp();
+              });
+              return;
+            }
+          });
+        };
+      });
+    }
 
     return (
         <main className="rumors">
@@ -88,8 +143,8 @@ function Login(props) {
               </button>
             </div>
           </form>
+          <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
         </main>
     );
-};
-
+}
 export default Login;
