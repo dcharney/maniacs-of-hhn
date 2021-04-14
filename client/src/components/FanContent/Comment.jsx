5f9bb3ab214a96b63c121c2221b63f73305aea72
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-04-14 17:07:32 -0400

    added comment box

--- client/src/components/FanContent/Comment.jsx
+++ client/src/components/FanContent/Comment.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 
 
-const Comment = props => {
+const Comment = (props) => {
     return (
+        <>
+        <section className="create-post">
+          <form onSubmit={props.savePost}>
 
-
-<section>
-
-
-
-
-
-            <h1>Welcome Home :)</h1>
-        
-
-
-
-
-
-</section>
-
-
-        
-
-    )
-};
+            <br />
+            <br />
+            <textarea
+              onChange={props.savePostContentToState}
+              placeholder="Descriptions"
+              rows="8"
+              cols="41"
+              required
+              ref={props.getContent}
+            ></textarea>
+            <br />
+            <br />
+            <section className="button-wrapper">
+            <button className="button">Submit</button></section>
+          </form>
+          </section>
+        </>
+      );
+    };
      export default Comment;
 
 
